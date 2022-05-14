574c56909847a6cd071af916ff59585be03cb8a5
Nelly Nikitina <100719863+nelllyai@users.noreply.github.com>
2022-05-14 23:02:42 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,3 +1,17 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include "../include/task.h"
+
+TEST(task1, test1) {
+	  int cashboxes = 2;
+    int intensity = 30;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+  	int result = supermarket.getAmountOfNotServedCustomers();
+  	EXPECT_EQ(0, result);
+}
