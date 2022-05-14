298a9eadf478f8a3e39c7949f940d63f37da9ffd
Nelly Nikitina <100719863+nelllyai@users.noreply.github.com>
2022-05-15 00:58:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -27,5 +27,102 @@ TEST(TEST2, test2) {
     supermarket.startShopping();
   
     int result = supermarket.getAmountOfServedCustomers();
-    EXPECT_EQ(4, result);
+    ASSERT_EQ(result, 4);
+}
+
+TEST(TEST3, test3) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+    double result = supermarket.getAverageQueueLength();
+    ASSERT_EQ(result, 3);
+}
+
+TEST(TEST4, test4) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+    double result = supermarket.getAverageClientTime();
+    ASSERT_TRUE(result > 30);
+}
+
+TEST(TEST5, test5) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+    
+    ASSERT_TRUE(supermarket.getAverageCashboxWorktime() > supermarket.getAverageClientTime());
+}
+
+TEST(TEST6, test6) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+    double result = supermarket.getAverageCashboxDowntime();
+    ASSERT_EQ(result, 0);
+}
+
+TEST(TEST7, test7) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+    double result = supermarket.getPrej();
+    ASSERT_EQ(result, 0.2);
+}
+
+TEST(TEST8, test8) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+    double result = supermarket.getQ();
+    ASSERT_EQ(result, 0.8);
+}
+
+TEST(TEST9, test9) {
+    int cashboxes = 1;
+    int intensity = 10;
+    int speed = 10;
+    int averageProducts = 3;
+    int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+  
+    double result = supermarket.getA();
+    ASSERT_EQ(result, 8);
 }
